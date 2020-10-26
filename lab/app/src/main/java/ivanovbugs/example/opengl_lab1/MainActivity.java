9412fe959d6f921214ca044b6d0476d00f06b813
l1fad <lexa77159@gmail.com>
2020-10-26 16:22:16 +0700

    add cube

--- lab/app/src/main/java/ivanovbugs/example/opengl_lab1/MainActivity.java
+++ lab/app/src/main/java/ivanovbugs/example/opengl_lab1/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends AppCompatActivity
         g.setRenderer(new MyRenderer());
         g.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
         setContentView(g);
-
     }
 
 
--- lab/app/src/main/java/ivanovbugs/example/opengl_lab1/MyRenderer.java
+++ lab/app/src/main/java/ivanovbugs/example/opengl_lab1/MyRenderer.java
@@ -2,6 +2,8 @@ package ivanovbugs.example.opengl_lab1;
 
 import android.opengl.GLSurfaceView;
 import android.opengl.GLSurfaceView.Renderer;
+import android.opengl.GLU;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 import static android.opengl.GLES20.glClearColor;
@@ -15,49 +17,44 @@ import java.nio.FloatBuffer;
 
 public class MyRenderer implements Renderer
 {
-    float []a = new float[]
-    {
-        -1,1,0,
-        -1,-1,0,
-        1,-1,0,
-        1,1,0
-    };
-    FloatBuffer f;
-    ByteBuffer b;
+    private Cube mCube = new Cube();
+    private float mCubeRotation;
 
-    public MyRenderer(){
-        b=ByteBuffer.allocateDirect(4*3*4);
-        b.order(ByteOrder.nativeOrder());
-        f=b.asFloatBuffer();
-        f.put(a);
-        f.position(0);
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
+        gl.glClearDepthf(1.0f);
+        gl.glEnable(GL10.GL_DEPTH_TEST);
+        gl.glDepthFunc(GL10.GL_LEQUAL);
+        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);
+
     }
 
     @Override
-    public void onSurfaceCreated(GL10 gl, EGLConfig config)
-    {
-    }
-
-    @Override
-    // Set the OpenGL viewport to fill the entire surface.
-    public void onSurfaceChanged(GL10 gl, int width, int height)
-    {
-    }
-
-    @Override
-    // Clear the rendering surface.
-    public void onDrawFrame(GL10 gl)
-    {
-        gl.glClearColor(0,0,0,1);
-        gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+    public void onDrawFrame(GL10 gl) {
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
         gl.glLoadIdentity();
-        gl.glTranslatef(0,0,-1);
-        gl.glScalef(0.5f,0.5f,0.5f);
-        gl.glColor4f(1,1,1,1);
-        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-        gl.glVertexPointer(3,GL10.GL_FLOAT,0,f);
-        gl.glDrawArrays(GL10.GL_TRIANGLE_FAN,0,4);
-        gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+
+        gl.glTranslatef(0.0f, 0.0f, -10.0f);
+        gl.glRotatef(mCubeRotation, 1.0f, 1.0f, 1.0f);
+
+        mCube.draw(gl);
+
+        gl.glLoadIdentity();
+
+        mCubeRotation -= 0.15f;
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        gl.glViewport(0, 0, width, height);
+        gl.glMatrixMode(GL10.GL_PROJECTION);
+        gl.glLoadIdentity();
+        GLU.gluPerspective(gl, 45.0f, (float)width / (float)height, 0.1f, 100.0f);
+        gl.glViewport(0, 0, width, height);
+
+        gl.glMatrixMode(GL10.GL_MODELVIEW);
+        gl.glLoadIdentity();
     }
 
 }
